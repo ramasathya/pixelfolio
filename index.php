b5f819fa31e2fe72bdc9661293848acf8daed268
ramasathya <ramasatya112@gmail.com>
2025-01-18 02:39:54 +0800

    index dan navbar

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Pixelfolio</title>
+    <link rel="stylesheet" href="css/styles.css">
+</head>
+<body>
+    <!-- Navbar -->
+    <div id="navbar">
+        <div class="title">
+        <a href="index.php" class="logo">Pixelfolio</a>
+        </div>
+        <div class="search-form">
+            <form method="GET" action="index.php">
+                <input type="text" name="search" id="search" placeholder="Search" value="">
+                <button type="submit" class="search-button">Search</button>
+            </form>
+        </div>
+        <div class="loginbar">
+                <a href="login.php" class="logbutton">Login</a>
+                <a href="register.php" class="logbutton">Register</a>
+        </div>
+    </div>
+</body>
